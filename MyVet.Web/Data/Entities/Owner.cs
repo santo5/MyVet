9cebe5c3a12a990e3259fa0fff131a0484481055
USUARIO <USUARIO@DESKTOP-KQ4THBV>
2020-03-21 15:44:44 -0500

    Agregar user entities

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    //Clase de propietario para la tabla de la base de datos
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<Pet> Pets { get; set; }
+
+        public ICollection<Agenda> Agendas { get; set; }
+    }
+}
